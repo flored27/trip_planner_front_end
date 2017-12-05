b65f4df5f48af7500d3f74a9b4b7aab40ce15f7a
github username <github email address>
2017-12-05 09:58:45 -0500

    added some logic

--- index.js
+++ index.js
@@ -5,12 +5,21 @@ function getItineraries(){
 
 //login
 let loginForm = document.getElementById("login-form")
+let loginButton = document.getElementById("loginButton")
+let emailForm = document.getElementById("email")
+let username = document.getElementById("username")
 let emailBox = document.createElement("div")
-loginForm.appendChild(emailBox)
-emailBox.innerHTML = `
-<label>Email</label>
-<input></input>
-<label>Password</label>
-<input></input>`
+loginButton.addEventListener('click', function(event) {
+  event.preventDefault()
+  loginForm.innerHTML = null
+  
+
+})
+// loginForm.appendChild(emailBox)
+// emailBox.innerHTML = `
+// <label>Email</label>
+// <input></input>
+// <label>Password</label>
+// <input></input>`
 
 let createItinerary = document.createElement("button")
