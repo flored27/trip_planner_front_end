fe0f53bdb5de62945587687be7dec4e23de2ccce
Abe Rudy <aberudy@gmail.com>
2017-12-06 13:47:45 -0500

    fixed new itinerary listing on page, changed listgroup var

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let emailForm = document.getElementById("email")
 let username = document.getElementById("username")
 let emailBox = document.createElement("div")
 let sideBarAppend = document.getElementById('side-bar')
-let listgroup = document.getElementsByClassName('list-group')
+let listgroup = document.getElementById('itin-container')
 let placeWhereItineraryLoads = document.getElementById('itinerary-detail')
 let createButton = document.getElementById("createButton")
 let searchButton = document.getElementById("searchButton")
@@ -14,7 +14,6 @@ const itineraryURL = 'http://localhost:3000/api/v1/itineraries'
 const locationURL = 'http://localhost:3000/api/v1/locations'
 
 document.addEventListener('DOMContentLoaded', function() {
-
   //search button stuff from the nav bar
   searchButton.addEventListener('click', function(event){
     event.preventDefault()
@@ -71,29 +70,26 @@ document.addEventListener('DOMContentLoaded', function() {
 
     let userId = parseInt(document.querySelector('.list-group-item').className.split(" ")[1])
     let submitForm = document.getElementById('submitForm')
-      submitForm.addEventListener('click', (event) => {
-          event.preventDefault();
-          fetch(itineraryURL, {
-            method: 'POST',
-            body: JSON.stringify({user_id: userId,
-                                  name: itineraryName.value,
-                                  description: itineraryDescription.value,
-                                  start_date: startDate.value,
-                                  end_date: endDate.value
-            }),
-            headers: {'Content-Type': 'application/json',
-              'Accept': 'application/json'}
-          }).then(res => res.json()).then(trip => {
-            let userTrip = document.createElement('li')
-            userTrip.id = trip.name
-            userTrip.innerText = trip.name
-            userTrip.className = "list-group-item " + userId
-            listgroup[0].appendChild(userTrip)
-          })
-        })
-
+    submitForm.addEventListener('click', (event) => {
+      while (listgroup.hasChildNodes()) {
+        listgroup.removeChild(listgroup.lastChild);
+      }
+      event.preventDefault();
+      fetch(itineraryURL, {
+        method: 'POST',
+        body: JSON.stringify({user_id: userId,
+          name: itineraryName.value,
+          description: itineraryDescription.value,
+          start_date: startDate.value,
+          end_date: endDate.value
+        }),
+        headers: {'Content-Type': 'application/json',
+          'Accept': 'application/json'}
+      }).then(res => res.json()).then(data => {
+        fetch(userURL).then(data => data.json()).then(user => displayItinerary(user))
+      })
     })
-
+  })
 
   //
   let addLocation = document.getElementById("addLocation")
@@ -164,7 +160,7 @@ document.addEventListener('DOMContentLoaded', function() {
   fetch(userURL).then(data => data.json()).then(user => userData(user))
 
   // code for loading users itineraries in side bar
-  const userData = (user) => {
+  function userData (user) {
     loginButton.addEventListener('click', function(event) {
       event.preventDefault()
       document.getElementById("username").style.display="none";
@@ -172,55 +168,59 @@ document.addEventListener('DOMContentLoaded', function() {
       createButton.style.display="block";
       document.getElementById("addLocation").style.display="block";
       //filters user JSON object to return record that is equal to the user input for email
-      let userFilter = user.filter(function (user) {
-        return user.email === emailForm.value
-      })
+      displayItinerary(user)
+    })
+  }
+  function displayItinerary(user){
+    let userFilter = user.filter(function (user) {
+      return user.email === emailForm.value
+    })
 
-      //creates the actual list elements with an id equal to the trip name, etc to show up in the left hand bar ofthe page
-      userFilter[0].user_trips.forEach(trip => {
-        let userTrip = document.createElement('li')
-        userTrip.id = trip.name
-        userTrip.innerText = trip.name
-        userTrip.className = "list-group-item " + userFilter[0].id
-        listgroup[0].appendChild(userTrip)
+    //creates the actual list elements with an id equal to the trip name, etc to show up in the left hand bar ofthe page
+    userFilter[0].user_trips.forEach(trip => {
+      let userTrip = document.createElement('li')
+      userTrip.id = trip.name
+      userTrip.innerText = trip.name
+      userTrip.className = "list-group-item " + userFilter[0].id
+      // debugger
+      listgroup.appendChild(userTrip)
 
-        //loads itinerary information when you click on that item in the side bar
-        userTrip.addEventListener('click', function(event) {
-          event.preventDefault()
-          //removes childNodes so when you click a new itinerary the previous itinerary is removed
-          while (placeWhereItineraryLoads.hasChildNodes()) {
-            placeWhereItineraryLoads.removeChild(placeWhereItineraryLoads.lastChild);
-          }
-          //loads itinerary information..continued
-          fetch('http://localhost:3000/api/v1/itineraries').then(data => data.json()).then(itinerary => {
-            // filters for a particular user
-            let itineraryFilter = itinerary.filter(itin => {
-              return itin.name === userTrip.id
-            })
-            // creating the place on the page where the itinerary info loads
-            let itineraryArea = document.createElement('div')
-            placeWhereItineraryLoads.appendChild(itineraryArea)
-            itineraryArea.innerHTML = `
-            <div class="card" style="text-align: center; padding-top: 25px; width: 400px; height: 200px; opacity: .75">
-            <h6>This Trip:</h6>
-            <h1> ${itineraryFilter[0].name} </h1>
-            <p> ${itineraryFilter[0].description}</p>
-            <h6>Things to do:</h6>
+      //loads itinerary information when you click on that item in the side bar
+      userTrip.addEventListener('click', function(event) {
+        event.preventDefault()
+        //removes childNodes so when you click a new itinerary the previous itinerary is removed
+        while (placeWhereItineraryLoads.hasChildNodes()) {
+          placeWhereItineraryLoads.removeChild(placeWhereItineraryLoads.lastChild);
+        }
+        //loads itinerary information..continued
+        fetch('http://localhost:3000/api/v1/itineraries').then(data => data.json()).then(itinerary => {
+          // filters for a particular user
+          let itineraryFilter = itinerary.filter(itin => {
+            return itin.name === userTrip.id
+          })
+          // creating the place on the page where the itinerary info loads
+          let itineraryArea = document.createElement('div')
+          placeWhereItineraryLoads.appendChild(itineraryArea)
+          itineraryArea.innerHTML = `
+          <div class="card" style="text-align: center; padding-top: 25px; width: 600px; height: 200px; opacity: .75">
+          <h6>This Trip:</h6>
+          <h1> ${itineraryFilter[0].name} </h1>
+          <p> ${itineraryFilter[0].description}</p>
+          <h6>Things to do:</h6>
+          </div>`
+          itineraryFilter[0].destinations.forEach(destination => {
+            let locationArea = document.createElement('div')
+            locationArea.className = "card"
+            locationArea.style = "margin-left: 25px; width: 550px; height: 225px; opacity: .75"
+            locationArea.innerHTML = `
+            <div class="card-body">
+            <h4 style="text-align:center">${destination.name}</h6>
+            <br>
+            <h6 style="text-align:center">${destination.street_address}</h4>
+            <h5 style="text-align:center">${destination.city}, ${destination.state} ${destination.zip}</h5>
+            <br>
             </div>`
-            itineraryFilter[0].destinations.forEach(destination => {
-              let locationArea = document.createElement('div')
-              locationArea.className = "card"
-              locationArea.style = "margin-left: 25px; width: 350px; height: 225px; opacity: .75"
-              locationArea.innerHTML = `
-              <div class="card-body">
-              <h4 style="text-align:center">${destination.name}</h6>
-              <br>
-              <h6 style="text-align:center">${destination.street_address}</h4>
-              <h5 style="text-align:center">${destination.city}, ${destination.state} ${destination.zip}</h5>
-              <br>
-              </div>`
-              itineraryArea.appendChild(locationArea)
-            })
+            itineraryArea.appendChild(locationArea)
           })
         })
       })
---
+++ test.js
@@ -0,0 +1,54 @@
+function displayItinerary(user){
+  let userFilter = user.filter(function (user) {
+    return user.email === emailForm.value
+  })
+
+  //creates the actual list elements with an id equal to the trip name, etc to show up in the left hand bar ofthe page
+  userFilter[0].user_trips.forEach(trip => {
+    let userTrip = document.createElement('li')
+    userTrip.id = trip.name
+    userTrip.innerText = trip.name
+    userTrip.className = "list-group-item " + userFilter[0].id
+    listgroup[0].appendChild(userTrip)
+
+    //loads itinerary information when you click on that item in the side bar
+    userTrip.addEventListener('click', function(event) {
+      event.preventDefault()
+      //removes childNodes so when you click a new itinerary the previous itinerary is removed
+      while (placeWhereItineraryLoads.hasChildNodes()) {
+        placeWhereItineraryLoads.removeChild(placeWhereItineraryLoads.lastChild);
+      }
+      //loads itinerary information..continued
+      fetch('http://localhost:3000/api/v1/itineraries').then(data => data.json()).then(itinerary => {
+        // filters for a particular user
+        let itineraryFilter = itinerary.filter(itin => {
+          return itin.name === userTrip.id
+        })
+        // creating the place on the page where the itinerary info loads
+        let itineraryArea = document.createElement('div')
+        placeWhereItineraryLoads.appendChild(itineraryArea)
+        itineraryArea.innerHTML = `
+        <div class="card" style="text-align: center; padding-top: 25px; width: 400px; height: 200px; opacity: .75">
+        <h6>This Trip:</h6>
+        <h1> ${itineraryFilter[0].name} </h1>
+        <p> ${itineraryFilter[0].description}</p>
+        <h6>Things to do:</h6>
+        </div>`
+        itineraryFilter[0].destinations.forEach(destination => {
+          let locationArea = document.createElement('div')
+          locationArea.className = "card"
+          locationArea.style = "margin-left: 25px; width: 350px; height: 225px; opacity: .75"
+          locationArea.innerHTML = `
+          <div class="card-body">
+          <h4 style="text-align:center">${destination.name}</h6>
+          <br>
+          <h6 style="text-align:center">${destination.street_address}</h4>
+          <h5 style="text-align:center">${destination.city}, ${destination.state} ${destination.zip}</h5>
+          <br>
+          </div>`
+          itineraryArea.appendChild(locationArea)
+        })
+      })
+    })
+  })
+}
